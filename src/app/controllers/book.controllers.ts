30ec5fd74a469f3824ed9d72c26cf7c5adf6f88f
Rafsan12 <rafsan.web.2022@gmail.com>
2025-06-29 20:59:20 +0600

    Get All books api complete

--- src/app/controllers/book.controllers.ts
+++ src/app/controllers/book.controllers.ts
@@ -3,7 +3,6 @@ import { Book } from "../models/books.models";
 export const bookRouter = express.Router();
 
 // Create a book
-
 bookRouter.post("/create-book", async (req: Request, res: Response) => {
   const book = await Book.create(req.body);
   res.status(201).json({
@@ -11,4 +10,32 @@ bookRouter.post("/create-book", async (req: Request, res: Response) => {
     message: "Book created successfully",
     data: book,
   });
+});
+
+// Get All books
+bookRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const {
+      filter,
+      sortBy = "createdAt",
+      sort = "desc",
+      limit = 10,
+    } = req.query;
+    const query: any = {};
+    if (filter) {
+      query.genre = filter;
+    }
+
+    const books = await Book.find(query)
+      .sort({ [sortBy as string]: sort === "asc" ? 1 : -1 })
+      .limit(Number(limit));
+
+    res.json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });

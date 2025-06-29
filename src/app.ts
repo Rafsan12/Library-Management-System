f11f4d90b47299cec3b5ee21efe4d17f3733f322
Rafsan12 <rafsan.web.2022@gmail.com>
2025-06-29 20:04:41 +0600

    Library management project setup :tada:

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/books", bookRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Library Management APP");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const PORT = 5000;
+
+let server;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Library:OcZ9SZJHVwofI5uE@cluster0.bytzc92.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("Connected to MongoDB Using Mongoose!!");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();

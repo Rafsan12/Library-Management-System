907c1064e37f53b9343a2d1e7b44d3294025712a
Rafsan12 <rafsan.web.2022@gmail.com>
2025-06-29 22:25:07 +0600

    books api complete

--- src/app/controllers/book.controllers.ts
+++ src/app/controllers/book.controllers.ts
@@ -1,19 +1,28 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
 import { Book } from "../models/books.models";
+
 export const bookRouter = express.Router();
 
 // Create a book
-bookRouter.post("/create-book", async (req: Request, res: Response) => {
-  const book = await Book.create(req.body);
-  res.status(201).json({
-    success: true,
-    message: "Book created successfully",
-    data: book,
-  });
-});
+bookRouter.post(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const book = await Book.create(req.body);
+      res.status(201).json({
+        success: true,
+        message: "Book created successfully",
+        data: book,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-// Get All books
-bookRouter.get("/", async (req: Request, res: Response) => {
+// Get all books
+bookRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const {
       filter,
@@ -21,6 +30,7 @@ bookRouter.get("/", async (req: Request, res: Response) => {
       sort = "desc",
       limit = 10,
     } = req.query;
+
     const query: any = {};
     if (filter) {
       query.genre = filter;
@@ -36,51 +46,95 @@ bookRouter.get("/", async (req: Request, res: Response) => {
       data: books,
     });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-// Get Single book
-bookRouter.get("/:bookId", async (req: Request, res: Response) => {
-  try {
-    const book = await Book.findById(req.params.bookId);
-    res.json({
-      success: true,
-      message: "Book retrieved successfully",
-      data: book,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+// Get single book by ID
+bookRouter.get(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const { bookId } = req.params;
+      if (!mongoose.isValidObjectId(bookId)) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid book ID",
+        });
+      }
+      const book = await Book.findById(bookId);
 
-// Update Book
-bookRouter.put("/:bookId", async (req: Request, res: Response) => {
-  try {
-    const book = await Book.findByIdAndUpdate(req.params.bookId, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.json({
-      success: true,
-      message: "Book updated successfully",
-      data: book,
-    });
-  } catch (error) {
-    console.log(error);
+      res.json({
+        success: true,
+        message: "Book retrieved successfully",
+        data: book,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
+
+// Update book -
+bookRouter.put(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const { bookId } = req.params;
+
+      if (!mongoose.isValidObjectId(bookId)) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid book ID",
+        });
+      }
+
+      const book = await Book.findByIdAndUpdate(bookId, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.json({
+        success: true,
+        message: "Book updated successfully",
+        data: book,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 // Delete book
-bookRouter.delete("/:bookId", async (req: Request, res: Response) => {
-  try {
-    const book = await Book.findByIdAndDelete(req.params.bookId);
-    res.json({
-      success: true,
-      message: "Book deleted successfully",
-      data: null,
-    });
-  } catch (error) {
-    console.log(error);
+bookRouter.delete(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const { bookId } = req.params;
+
+      if (!mongoose.isValidObjectId(bookId)) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid book ID",
+        });
+      }
+
+      const book = await Book.findByIdAndDelete(bookId);
+
+      if (!book) {
+        return res.status(404).json({
+          success: false,
+          message: "Book not found",
+        });
+      }
+
+      res.json({
+        success: true,
+        message: "Book deleted successfully",
+        data: null,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
